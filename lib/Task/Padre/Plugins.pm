d4c6bf1f549e0cf826fec717d104b05e36bc8d65
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2008-11-12 11:24:41 +0000

    Import of SZABGAB/Task-Padre-Plugins-0.03 from CPAN.
    gitpan-cpan-distribution: Task-Padre-Plugins
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SZABGAB/Task-Padre-Plugins-0.03.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

--- lib/Task/Padre/Plugins.pm
+++ lib/Task/Padre/Plugins.pm
@@ -9,11 +9,11 @@ Task::Padre::Plugins - Get many Plugins of Padre at once
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
